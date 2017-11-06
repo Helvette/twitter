07e297d10039ef98f695d1af7c251495745ae804
Helvette <malastrugo@live.cl>
2017-11-06 11:36:08 -0300

    Tercer commit Twitter. Falta salto de línea y detalles CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
-var button = document.getElementsByTagName("button")[0]; // botón
-var textarea = document.getElementsByTagName("textarea")[0]; // formulario
+var button = document.getElementsByTagName("button")[0]; // Botón
+var textarea = document.getElementsByTagName("textarea")[0]; // Formulario
+var span = document.getElementById("contador"); // Contador
 
 // Evento para que el botón esté desactivado cuando no hay nada escrito
 textarea.addEventListener("focus", function(){
@@ -8,8 +9,6 @@ textarea.addEventListener("focus", function(){
 		button.setAttribute("disabled","disabled");
 	}
 })
-
-var span = document.getElementById("contador"); // Contador
 
 /* Evento para que cuente los carácteres disponibles en el formulario
 y  cambie el número del contador en base a ello */
@@ -46,23 +45,90 @@ y que además en él contenga el value del formulario al momento de hacer click
 button.addEventListener("click", function(){
 	var texto = document.getElementsByTagName("textarea")[0].value;
 	if (texto.length > 0){ // Para que no se active el botón hasta escribir algo
-		var contenedorBotton = document.getElementById("contenedor-bottom");
+		var contenedorBottom = document.getElementById("contenedor-bottom");
 		var nuevoElemento = document.createElement("article");
 		var nuevoParrafo = document.createElement("p");
+		var span = document.getElementById("contador"); // Contador
+		var firstArticle = contenedorBottom.children[0];
 		texto = document.createTextNode(texto);
 		nuevoParrafo.appendChild(texto);
 		nuevoElemento.appendChild(nuevoParrafo);
-		contenedorBotton.appendChild(nuevoElemento);
+		nuevoElemento.classList.add("parrafo");
+		// Para que cada article se posicione por encima del anterior
+		contenedorBottom.insertBefore(nuevoElemento, firstArticle);
+
+		// Poniendo Nick
+		var name = document.createElement("span");
+		name.textContent = "Leticia Ferreira"
+		name.classList.add("name")
+		nuevoElemento.insertBefore(name, nuevoParrafo);
+
+		var nick = document.createElement("span");
+		nick.textContent = "@Helvette";
+		nuevoElemento.insertBefore(nick, nuevoParrafo);
+
+		// Ponieno íconos 
+		var heart = document.createElement("i");
+		heart.classList.add("fa", "fa-heart", "heart");
+		//creamos icono de basura
+		var trash = document.createElement("i");
+		trash.classList.add("fa", "fa-trash", "trash");
+		nuevoElemento.appendChild(heart);
+		nuevoElemento.appendChild(trash);
+
+		// Capturando hora
 		var time = new Date();
-		time = time.getHours() + ":" + time.getMinutes();
+		var hours = time.getHours();
+		var minutes = time.getMinutes();
+		// En caso de que la hora o los minutos contengan solo un dígito
+		if(hours < 10){
+			hours = "0" + hours;
+		}
+		if(minutes < 10){
+			minutes = "0" + minutes;
+		}
+		time = hours + ":" + minutes;
 		var hora = document.createElement("p");
 		var horaContenido = document.createTextNode(time);
 		hora.appendChild(horaContenido);
-		nuevoElemento.appendChild(hora);
+		nuevoElemento.insertBefore(hora, nuevoParrafo);
+
+		// Limpiar textarea
+		document.getElementsByTagName("textarea")[0].value = "";
+		// Reiniciar botón
+		span.textContent = "140"
+		// Deshabilitar botón
+		button.setAttribute("disabled","disabled");
 	}
 })
 
 
+
+
+/* textarea.addEventListener("keyup", function (){
+	if (textarea.value.split("\n").length > 3){
+		textarea.rows = textarea.rows + 1;
+	}else {
+		textarea.rows = 3;
+	}
+})
+
+
+/* $(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+        this.rows = minRows + rows;
+    });
+
+*
 
 
 
