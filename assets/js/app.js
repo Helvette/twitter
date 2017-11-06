15fa91eaa36af44e99695c76990bb734a223341f
Helvette <malastrugo@live.cl>
2017-11-06 16:58:26 -0300

    Funcionalidad lista! Solo falta afinar estilos

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@ var span = document.getElementById("contador"); // Contador
 
 // Evento para que el botón esté desactivado cuando no hay nada escrito
 textarea.addEventListener("focus", function(){
-	var textarea = document.getElementsByTagName("textarea")[0];
 	if (textarea.value.length === 0){
 		button.setAttribute("disabled","disabled");
 	}
@@ -13,8 +12,6 @@ textarea.addEventListener("focus", function(){
 /* Evento para que cuente los carácteres disponibles en el formulario
 y  cambie el número del contador en base a ello */
 textarea.addEventListener("keyup", function(){
-	var textarea = document.getElementsByTagName("textarea")[0];
-	var span = document.getElementById("contador");
 	var newCounter = textarea.value.length; // Longitud del contenido
 	newCounter = 140 - newCounter; // Nuevo contador
 	span.textContent = newCounter; // Cambiando el contenido del elemento span
@@ -48,33 +45,44 @@ button.addEventListener("click", function(){
 		var contenedorBottom = document.getElementById("contenedor-bottom");
 		var nuevoElemento = document.createElement("article");
 		var nuevoParrafo = document.createElement("p");
-		var span = document.getElementById("contador"); // Contador
 		var firstArticle = contenedorBottom.children[0];
 		texto = document.createTextNode(texto);
 		nuevoParrafo.appendChild(texto);
+		nuevoParrafo.classList.add("parrafo");
 		nuevoElemento.appendChild(nuevoParrafo);
-		nuevoElemento.classList.add("parrafo");
 		// Para que cada article se posicione por encima del anterior
 		contenedorBottom.insertBefore(nuevoElemento, firstArticle);
 
-		// Poniendo Nick
+		// Poniendo nombre y nick antes del estado
 		var name = document.createElement("span");
 		name.textContent = "Leticia Ferreira"
 		name.classList.add("name")
 		nuevoElemento.insertBefore(name, nuevoParrafo);
 
 		var nick = document.createElement("span");
-		nick.textContent = "@Helvette";
+		nick.textContent = " @Helvette ";
+		nick.classList.add("nick");
 		nuevoElemento.insertBefore(nick, nuevoParrafo);
 
+		// Poniendo imagen
+		var imagen = document.createElement("img");
+		imagen.classList.add("imagen");
+		nuevoElemento.insertBefore(imagen, name);
+		imagen.setAttribute("src", "assets/images/profile-photo.jpg");
+
 		// Ponieno íconos 
+		// corazón
 		var heart = document.createElement("i");
 		heart.classList.add("fa", "fa-heart", "heart");
-		//creamos icono de basura
-		var trash = document.createElement("i");
-		trash.classList.add("fa", "fa-trash", "trash");
+		// retweet
+		var reTweet = document.createElement("i");
+		reTweet.classList.add("fa", "fa-retweet", "fa-retweet");
+		// responder
+		var message = document.createElement("i");
+		message.classList.add("fa", "fa-comment", "comment");
 		nuevoElemento.appendChild(heart);
-		nuevoElemento.appendChild(trash);
+		nuevoElemento.appendChild(reTweet);
+		nuevoElemento.appendChild(message);
 
 		// Capturando hora
 		var time = new Date();
@@ -87,14 +95,17 @@ button.addEventListener("click", function(){
 		if(minutes < 10){
 			minutes = "0" + minutes;
 		}
-		time = hours + ":" + minutes;
+		time = "Posted on " + hours + ":" + minutes;
 		var hora = document.createElement("p");
 		var horaContenido = document.createTextNode(time);
+		hora.classList.add("hora");
 		hora.appendChild(horaContenido);
 		nuevoElemento.insertBefore(hora, nuevoParrafo);
 
 		// Limpiar textarea
-		document.getElementsByTagName("textarea")[0].value = "";
+		textarea.value = "";
+		// Regresar textarea a su tamaño original
+		textarea.style.height = "100px"
 		// Reiniciar botón
 		span.textContent = "140"
 		// Deshabilitar botón
@@ -102,41 +113,11 @@ button.addEventListener("click", function(){
 	}
 })
 
-
-
-
-/* textarea.addEventListener("keyup", function (){
-	if (textarea.value.split("\n").length > 3){
-		textarea.rows = textarea.rows + 1;
-	}else {
-		textarea.rows = 3;
-	}
+// Función para que el textarea se agrande automáticamente en base a su contenido
+textarea.addEventListener("keydown", function(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
 })
-
-
-/* $(document)
-    .one('focus.autoExpand', 'textarea.autoExpand', function(){
-        var savedValue = this.value;
-        this.value = '';
-        this.baseScrollHeight = this.scrollHeight;
-        this.value = savedValue;
-    })
-    .on('input.autoExpand', 'textarea.autoExpand', function(){
-        var minRows = this.getAttribute('data-min-rows')|0, rows;
-        this.rows = minRows;
-        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
-        this.rows = minRows + rows;
-    });
-
-*
-
-
-
-
-/* textarea.addEventListener("keyup",function (){
-	var textarea = document.getElementsByTagName("textarea")[0]; // formulario
-	
-	if (textarea.rows > 10){
-		textarea.rows = textarea.rows + 1;
-	}
-}) */
